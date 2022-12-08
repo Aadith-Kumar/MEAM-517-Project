6dcd7421ff8c8cc0aa1e13760a4849075a34cb0c
Rithwik Udayagiri <rithwik.math@gmail.com>
2022-12-08 03:11:07 -0500

    Poster graphs

--- graphs/plot_paths.py
+++ graphs/plot_paths.py
@@ -9,9 +9,11 @@ w1 = np.load(file_waypoints+"1.npy")
 x2 = np.load(file_path+"2.npy")
 w2 = np.load(file_waypoints+"2.npy")
 
-f = plt.figure()
-f.set_figwidth(10)
-f.set_figheight(10)
+fig, ax = plt.subplots()
+fig.set_figwidth(10)
+fig.set_figheight(10)
+
+plt.rcParams.update({'font.size': 22})
 
 plt.plot(x1[:, 0], x1[:, 1], "b-", label="Robot 1 MPC path")
 plt.plot(w1[:, 0], w1[: ,1], "rx", label="Robot 1 A* Waypoints")
@@ -19,11 +21,11 @@ plt.plot(w1[:, 0], w1[: ,1], "rx", label="Robot 1 A* Waypoints")
 plt.plot(x2[:, 0], x2[:, 1], "m-", label="Robot 2 MPC path")
 plt.plot(w2[:, 0], w2[: ,1], "gx", label="Robot 2 A* Waypoints")
 
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("MPC paths")
-plt.legend()
-plt.grid()
+ax.set_xlabel("x", fontsize=20)
+ax.set_ylabel("y", fontsize=20)
+ax.set_title("MPC paths", fontsize=20)
+plt.legend(loc='best', prop={'size': 15})
+ax.grid()
 
 file_path = "path_combined"
 plt.savefig(file_path+".png")
--- multi_sim_launch.sh
+++ multi_sim_launch.sh
@@ -16,6 +16,6 @@ gazebo --verbose -s libgazebo_ros_factory.so &
 # done
 
 ros2 run robot_spawner_pkg spawn_turtlebot robot1 robot1 0.0 0.0 0.1;
-ros2 run robot_spawner_pkg spawn_turtlebot robot2 robot2 5.0 5.0 0.1;
+ros2 run robot_spawner_pkg spawn_turtlebot robot2 robot2 5.0 0.0 0.1;
 
 #ros2 run mpc sample_state_update_code.py
