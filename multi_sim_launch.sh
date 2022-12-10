ae9f652bd00b7beb9c46d8852f21605abf068469
Rithwik Udayagiri <rithwik.math@gmail.com>
2022-12-10 01:34:20 -0500

    Chance 3 robots

--- multi_sim_launch.sh
+++ multi_sim_launch.sh
@@ -17,5 +17,6 @@ gazebo --verbose -s libgazebo_ros_factory.so &
 
 ros2 run robot_spawner_pkg spawn_turtlebot robot1 robot1 0.0 0.0 0.1;
 ros2 run robot_spawner_pkg spawn_turtlebot robot2 robot2 5.0 0.0 0.1;
+ros2 run robot_spawner_pkg spawn_turtlebot robot3 robot3 2.0 0.0 0.1;
 
 #ros2 run mpc sample_state_update_code.py
